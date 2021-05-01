839cdf7b773ea018159dbe2b8def1ac1631498d7
kevinchen2002 <kaifengchen2002@gmail.com>
2021-05-01 09:35:50 -0700

    hi

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
 import pyautogui
-print(pyautogui.position())
+dimensions = pyautogui.size()
+print("width ", dimensions[0], " height ", dimensions[1])
+pyautogui.moveTo(100, 100, duration=0.5)
+pyautogui.moveTo(200, 100, duration=0.5)
+pyautogui.moveTo(200, 200, duration=0.5)
+pyautogui.moveTo(100, 200, duration=0.5)
 
-print("Hello!")
+image = pyautogui.locateOnScreen("Capture.PNG")
+print(image)
+pyautogui.moveTo(image[0], image[1], duration=0.5)
+pyautogui.moveTo(image[0]+image[2], image[1], duration=0.5)
+pyautogui.moveTo(image[0]+image[2], image[1]+image[3], duration=0.5)
+pyautogui.moveTo(image[0], image[1]+image[3], duration=0.5)
+pyautogui.moveTo(image[0], image[1], duration=0.5)
