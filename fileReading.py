f21e5def1751e35aec80892c2a3c5a231eead070
Vinci Velasco <vvv@sfu.ca>
2021-05-01 11:28:02 -0700

    fileReading.py and input.txt

---
+++ fileReading.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import os
+
+def read_by_line(filename):
+    input_file = open(Path("./") / filename)
+    lines = input_file.readlines()
+    return lines
+
+lines = read_by_line("input.txt")
+print(lines)
