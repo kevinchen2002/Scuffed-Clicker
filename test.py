c2347cda178e57fa3448041d454c7662e443a1f4
kevinchen2002 <kaifengchen2002@gmail.com>
2021-05-01 13:06:13 -0700

    a

--- test.py
+++ test.py
@@ -5,11 +5,24 @@ pyautogui.moveTo(100, 100, duration=0.5)
 pyautogui.moveTo(200, 100, duration=0.5)
 pyautogui.moveTo(200, 200, duration=0.5)
 pyautogui.moveTo(100, 200, duration=0.5)
+pyautogui.move(-100, 0, duration=0.5)
 
 print ("Hello this is Josh")
 
 print("hello world")
-image = pyautogui.locateOnScreen("Capture.PNG")
+image = pyautogui.locateOnScreen("Capture.png")
 print(image)
 pyautogui.moveTo(image[0]+(image[2]/2), image[1]+(image[3]/2), duration=0.5)
+#pyautogui.click()
 pyautogui.click()
+
+# pyautogui.click('Capture.png')
+pyautogui.write(['backspace'])
+pyautogui.write("google.ca")
+pyautogui.write(['enter'])
+
+#returns the current mouse position
+#p = pyautogui.position()
+
+#you can use pyautogui.click(10, 10, button='left')
+
